6bdd37488a3d2ddb0000f9aa32fd72e836da0d0b
Kanem <kanemdeng@email.com>
2022-02-23 23:24:58 -0800

    added spinner2.js

---
+++ spinner2.js
@@ -0,0 +1,41 @@
+const spinner = (data) => {
+  let result = 0;
+  for (const change of data) {
+    setTimeout(()=> {
+      process.stdout.write(`\r${change}  `);
+    }, result += 200);
+  }
+};
+const signs = "|/-\\|/-\\|";
+spinner(signs);
+// setTimeout(()=> {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(()=> {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(()=> {
+//   process.stdout.write('\r\\   ');
+// },700);
+
+// setTimeout(()=> {
+//   process.stdout.write('\r|   ');
+// },900);
+
+// setTimeout(()=> {
+//   process.stdout.write('\r/   ');
+// }, 1200);
+
+// setTimeout(()=> {
+//   process.stdout.write('\r-   ');
+// }, 1400);
+
+// setTimeout(()=> {
+//   process.stdout.write('\r\\   ');
+// }, 1600);
+
+// setTimeout(()=> {
+//   process.stdout.write('\r|   ');
+// }, 1800);
